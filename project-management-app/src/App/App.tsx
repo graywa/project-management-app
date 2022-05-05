470e14df62b93e743940b05d44d30ac115e446ad
Alexander <renyreny125tt@gmail.com>
2022-05-05 21:29:01 +0300

    refactor: add settings module and scss

--- project-management-app/src/App/App.tsx
+++ project-management-app/src/App/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={styles.App}>
+      <header className={styles.App_header}>
         <h1>project-management-app</h1>
       </header>
     </div>
