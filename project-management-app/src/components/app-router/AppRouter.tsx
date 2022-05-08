4dffa6d60e5f17e8b0c95dde6e3a5fc5d083f258
graywa <74241140+graywa@users.noreply.github.com>
2022-05-08 14:31:18 +0300

    refactor: fix welcome page

--- project-management-app/src/components/app-router/AppRouter.tsx
+++ project-management-app/src/components/app-router/AppRouter.tsx
@@ -1,21 +1,41 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import LoginPage from '../../pages/login-page/LoginPage';
 import MainPage from '../../pages/main-page/MainPage';
 import WelcomePage from '../../pages/welcome-page/WelcomePage';
 
 const AppRouter = () => {
   const isAuth = false;
+  const { pathname } = useLocation();
 
-  return isAuth ? (
-    <Routes>
-      <Route path="/main" element={<MainPage />} />
-    </Routes>
-  ) : (
-    <Routes>
-      <Route path="/" element={<WelcomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-  );
+  const allowedPaths = ['/', '/login', '/main'];
+
+  const isAllowedPath = allowedPaths.some((el) => el === pathname);
+
+  if (isAuth && isAllowedPath)
+    return (
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="*" element={<Navigate to="/main" />} />
+      </Routes>
+    );
+
+  if (!isAuth && isAllowedPath)
+    return (
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
+
+  if (!isAllowedPath)
+    return (
+      <Routes>
+        <Route path="*" element={<h1>Page 404</h1>} />
+      </Routes>
+    );
+
+  return null;
 };
 
 export default AppRouter;
