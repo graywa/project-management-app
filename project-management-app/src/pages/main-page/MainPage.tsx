681480f74180ce492e568583fd3098473ed704ce
Alexander <renyreny125tt@gmail.com>
2022-05-09 18:53:55 +0300

    refactor: add button to home

--- project-management-app/src/pages/main-page/MainPage.tsx
+++ project-management-app/src/pages/main-page/MainPage.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './MainPage.module.scss';
 
 const MainPage = () => {
-  return <div className={styles.page}>Main Page</div>;
+  return (
+    <div className={styles.page}>
+      <h3>Main Page</h3>
+      <div>
+        <Link to="/">
+          <button>Go to Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
